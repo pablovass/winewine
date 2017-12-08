afbdf6fc36bdde6863ceae40a909f8b993334c60
pablovass <pablo@DESKTOP-9MU6T1S.(none)>
2017-12-08 17:53:49 -0300

    8/12/2017
    hoy

--- api/routes/mail.php
+++ api/routes/mail.php
@@ -1,6 +1,6 @@
 <?php
 
-
+//PrCnt de produt controler 
 use controller\MailController as PrCnt;
 use controller\AuthController;
 
@@ -11,3 +11,9 @@ $RestApi->get("/api/mail",function(){
 });
 
 ?>
+http://localhost/api/filter?category=2
+http://localhost/api/filter?category=2&max=1
+http://localhost/api/filter?category=2&max=5
+http://localhost/api/filter?category=2&max=22
+http://localhost/api/mail
+http://localhost/panel.html#section2
