34c994ad17c606d87e96eeac0a266c2689435b37
Pablo Vallejos <faktory.fm@live.com>
2019-02-17 12:03:18 -0300

    inserrt into
    insert

---
+++ api/db/insertIntoProductod.sql
@@ -0,0 +1,12 @@
+select * from producto
+
+
+INSERT INTO producto (nombre,descripcion,precio,cant_producto) 
+VALUES ('CALLEJON DEL CRIMEN GRAN RESERVA CABERNET SAUVIGNON','La nariz ofrece intensos aromas de frutos negros maduros con dedos de tabaco, cedro. 
+En boca es un vino de gran estructura con sabor a cassis y frutos.',498, 30);
+
+
+INSERT INTO producto (nombre,descripcion,precio,cant_producto) 
+VALUES ('DOÑA PAULA','Este Malbec presenta un estilo elegante y complejo, donde se destacan en nariz notas de frambuesa, arándano, violeta y especias que se integran armoniosamente a ciertos aromas minerales y de encina tostada. 
+En boca es un vino muy equilibrado, de gran cuerpo, concentrado y aterciopelado con un persistente final que recuerdan su paso por la madera.',402, 25);
+
