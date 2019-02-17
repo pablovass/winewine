c38d91ad0c36e06b52f94c295c73e09d6e959ad9
Pablo Vallejos <faktory.fm@live.com>
2019-02-16 22:17:07 -0300

    acomodando proyecto
    acomodando proyecto

--- api/db/init.sql
+++ api/db/init.sql
@@ -6,24 +6,15 @@ USE test23;
 CREATE TABLE Producto(
        id INT NOT NULL AUTO_INCREMENT,
        nombre VARCHAR(30) NOT NULL,
+       descripcion VARCHAR(1000) NOT NULL,
        precio FLOAT NOT NULL,
+       cant_producto INT NOT NULL,
        PRIMARY KEY (id)
 );
 
 
-INSERT INTO Producto (nombre,precio)
-VALUES ('Pantalon',320.30);
 
-INSERT INTO Producto (nombre,precio)
-VALUES ('Remera',200);
 
-CREATE TABLE Usuarios(
-       id INT NOT NULL AUTO_INCREMENT,
-       usuario VARCHAR(30) NOT NULL,
-       pass VARCHAR(30) NOT NULL,
-       role INT NOT NULL,
-       PRIMARY KEY (id)
-);
 
 CREATE TABLE Usuarios(
        id INT NOT NULL AUTO_INCREMENT,
@@ -35,6 +26,19 @@ CREATE TABLE Usuarios(
        role INT NOT NULL,
        PRIMARY KEY (id)
 );
+
+INSERT INTO Usuarios (usuario,pass,role) VALUES ('admin','admin',1);
+INSERT INTO Usuarios (usuario,pass,role) VALUES ('user','passw0rd',0);
+
+
+INSERT INTO Usuarios (usuario,pass,role, nombre,apellido,email)
+VALUES ('admin','admin',1, 'Pablo','Vass','pablo.vallejos@davinci.edu.ar');
+
+
+
+INSERT INTO Usuarios (usuario,pass,role, nombre,apellido,email)
+VALUES ('jose2017','123456',0, 'Jose','Hernan','jose.hernan@davinci.edu.ar');
+
 
 INSERT INTO Usuarios (usuario,pass,role) VALUES ('admin','admin',1);
 INSERT INTO Usuarios (usuario,pass,role) VALUES ('user','passw0rd',0);
