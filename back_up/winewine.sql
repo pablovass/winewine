deff139a659599dabf9627d573c83854820d471b
pablovass <faktory.fm@live.com>
2019-03-10 19:36:06 -0300

    actualizando la base de datos

--- back_up/winewine.sql
+++ back_up/winewine.sql
@@ -1,3 +1,4 @@
+/* retomando el proyecto la base de datos 10/3/2019 funciona */;
 CREATE DATABASE test23;
 GRANT USAGE ON *.* TO user23@localhost IDENTIFIED BY 'pass23';
 GRANT ALL PRIVILEGES ON test23.* TO user23@localhost ;
