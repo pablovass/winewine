3c10985e2cf9b08657dcfbced5f83db74756dd68
pablovass <faktory.fm@live.com>
2019-06-23 01:55:20 -0300

    guardando un back-up de productos

--- objectJs/producto.js
+++ objectJs/producto.js
@@ -1,4 +1,6 @@
+
 class Producto{
+
     constructor(id,nombre,descricion,precio,cantProducto,tipo){
         this.id=id;
         this.nombre=nombre;
@@ -7,54 +9,32 @@ class Producto{
         this.cantProducto=cantProducto;
         this.tipo=tipo;
     }
-    //set and get
-    // metodos ingresar producto extraer productos
 
 }
-//crear una Instacia 
-                            //id,nombre,descricion,precio,cantProducto,tipo
-//const vino = new Producto('Pablo','Vallejos',01,'pablovass',1500);
+
+let uri= 'http://localhost/api/product';
+let extension;
 
 function getPost(){
     return fetch('http://localhost/api/product');
 }
-//###################
 
- function hola(){ 
+ function allProduct(){ 
 getPost()
     // fetch('/api/product')
      .then((res)=>res.json())
      .then((data)=>{
        
-         let product=''
-         console.log(data) //para ver 
-         
-         data.forEach(function(prod) {
-            console.log(prod.nombre)
+         data.forEach(function(data) {
+            const product=new Producto(data.id,data.nombre,data.descripcion,data.precio,data.cantProducto,data.tipo)
+             
+            console.log(product.precio)
             
-             /*if (prod.tipo==1) {
-              
-                 product += `
-                      
-                      <div class="col-sm-6 col-md-4">
-                        <div class="thumbnail">
-                          <img src="/images/${prod.id}.jpg" alt="...">
-                          <div class="caption">
-                            <h3>${prod.nombre}</h3>
-                            <p><b>Oferta $${prod.precio}</b>  <span style="text-decoration: line-through;">Antes $${prod.precio}</span></p>
-                            <p><a style="margin-left: 67px;" href="producto_detalle.html?id=${prod.id}" class="btn btn-default" role="button">Comprar</a></p>
-                          </div>
-                        </div>
-                      </div>
-                    
-                      
-                      `;  
-             }*/
          });
-       //   document.getElementById('product').innerHTML=product;
          
         
      })
     }
- //window.onload=getPost
- hola()
+
+ allProduct()
+ 
