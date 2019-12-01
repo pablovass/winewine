bed7dd0816aa8fe8b5192d6b62412e2063a31880
pablovass <faktory.fm@live.com>
2019-12-01 14:33:08 -0300

    cambiando las rutas de las imagenes del home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -13,7 +13,7 @@ function mostrarHome(){
       <div class="col-sm-4">
         <p class="text-center"><strong>vino 1</strong></p><br>
         <a href="#demo" data-toggle="collapse">
-          <img src="images/1.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+          <img src="/src/images/1.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
         </a>
         <div id="demo" class="collapse">
           <p>Guitarist and Lead Vocalist</p>
@@ -24,7 +24,7 @@ function mostrarHome(){
       <div class="col-sm-4">
         <p class="text-center"><strong>vino 2</strong></p><br>
         <a href="#demo2" data-toggle="collapse">
-          <img src="images/10.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+          <img src="/src/images/2.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
         </a>
         <div id="demo2" class="collapse">
           <p>Drummer</p>
@@ -35,7 +35,7 @@ function mostrarHome(){
       <div class="col-sm-4">
         <p class="text-center"><strong>vino 3 </strong></p><br>
         <a href="#demo3" data-toggle="collapse">
-          <img src="images/11.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+          <img src="/src/images/3.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
         </a>
         <div id="demo3" class="collapse">
           <p>Bass player</p>
@@ -48,7 +48,7 @@ function mostrarHome(){
       <div class="col-sm-4">
         <p class="text-center"><strong>vino 4</strong></p><br>
         <a href="#demo4" data-toggle="collapse">
-          <img src="images/13.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+          <img src="/src/images/4.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
         </a>
         <div id="demo4" class="collapse">
           <p>Guitarist and Lead Vocalist</p>
@@ -59,7 +59,7 @@ function mostrarHome(){
       <div class="col-sm-4">
         <p class="text-center"><strong>vino 5</strong></p><br>
         <a href="#demo5" data-toggle="collapse">
-          <img src="images/15.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+          <img src="/src/images/5.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
         </a>
         <div id="demo5" class="collapse">
           <p>Drummer</p>
@@ -70,7 +70,7 @@ function mostrarHome(){
       <div class="col-sm-4">
         <p class="text-center"><strong>vino 6</strong></p><br>
         <a href="#demo6" data-toggle="collapse">
-          <img src="images/44.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+          <img src="/src/images/6.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
         </a>
         <div id="demo6" class="collapse">
           <p>Bass player</p>
@@ -97,7 +97,7 @@ function mostrarHome(){
       <div class="row text-center">
         <div class="col-sm-4">
           <div class="thumbnail">
-            <img src="images/7.jpg" alt="Paris" width="400" height="300">
+            <img src="/src/images/7.jpg" alt="Paris" width="400" height="300">
             <p><strong>Paris</strong></p>
             <p>Friday 27 November 2015</p>
             <button class="btn" data-toggle="modal" data-target="#myModal">Buy Tickets</button>
@@ -105,7 +105,7 @@ function mostrarHome(){
         </div>
         <div class="col-sm-4">
           <div class="thumbnail">
-            <img src="images/12.jpg" alt="New York" width="400" height="300">
+            <img src="/src/images/8.jpg" alt="New York" width="400" height="300">
             <p><strong>New York</strong></p>
             <p>Saturday 28 November 2015</p>
             <button class="btn" data-toggle="modal" data-target="#myModal">Buy Tickets</button>
@@ -113,7 +113,7 @@ function mostrarHome(){
         </div>
         <div class="col-sm-4">
           <div class="thumbnail">
-            <img src="images/44.jpg" alt="San Francisco" width="400" height="300">
+            <img src="/src/images/9.jpg" alt="San Francisco" width="400" height="300">
             <p><strong>San Francisco</strong></p>
             <p>Sunday 29 November 2015</p>
             <button class="btn" data-toggle="modal" data-target="#myModal">Buy Tickets</button>
@@ -125,7 +125,7 @@ function mostrarHome(){
       <div class="row text-center">
         <div class="col-sm-4">
           <div class="thumbnail">
-            <img src="/images/7.jpg" alt="Paris" width="400" height="300">
+            <img src="/src/images/10.jpg" alt="Paris" width="400" height="300">
             <p><strong>Paris</strong></p>
             <p>Friday 27 November 2015</p>
             <button class="btn" data-toggle="modal" data-target="#myModal">Buy Tickets</button>
@@ -133,7 +133,7 @@ function mostrarHome(){
         </div>
         <div class="col-sm-4">
           <div class="thumbnail">
-            <img src="/images/12.jpg" alt="New York" width="400" height="300">
+            <img src="/src/images/11.jpg" alt="New York" width="400" height="300">
             <p><strong>New York</strong></p>
             <p>Saturday 28 November 2015</p>
             <button class="btn" data-toggle="modal" data-target="#myModal">Buy Tickets</button>
@@ -141,7 +141,7 @@ function mostrarHome(){
         </div>
         <div class="col-sm-4">
           <div class="thumbnail">
-            <img src="images/13.jpg" alt="San Francisco" width="400" height="300">
+            <img src="/src/images/12.jpg" alt="San Francisco" width="400" height="300">
             <p><strong>San Francisco</strong></p>
             <p>Sunday 29 November 2015</p>
             <button class="btn" data-toggle="modal" data-target="#myModal">Buy Tickets</button>
@@ -168,7 +168,7 @@ function mostrarHome(){
       <div class="col-sm-4">
         <p class="text-center"><strong>vino 1</strong></p><br>
         <a href="#demo" data-toggle="collapse">
-          <img src="images/1.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+          <img src="/src/images/13.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
         </a>
         <div id="demo" class="collapse">
           <p>Guitarist and Lead Vocalist</p>
@@ -179,7 +179,7 @@ function mostrarHome(){
       <div class="col-sm-4">
         <p class="text-center"><strong>vino 2</strong></p><br>
         <a href="#demo2" data-toggle="collapse">
-          <img src="images/10.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+          <img src="/src/images/14.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
         </a>
         <div id="demo2" class="collapse">
           <p>Drummer</p>
@@ -190,7 +190,7 @@ function mostrarHome(){
       <div class="col-sm-4">
         <p class="text-center"><strong>vino 3 </strong></p><br>
         <a href="#demo3" data-toggle="collapse">
-          <img src="images/11.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+          <img src="/src/images/15.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
         </a>
         <div id="demo3" class="collapse">
           <p>Bass player</p>
@@ -203,7 +203,7 @@ function mostrarHome(){
       <div class="col-sm-4">
         <p class="text-center"><strong>vino 4</strong></p><br>
         <a href="#demo4" data-toggle="collapse">
-          <img src="images/13.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+          <img src="/src/images/16.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
         </a>
         <div id="demo4" class="collapse">
           <p>Guitarist and Lead Vocalist</p>
@@ -214,7 +214,7 @@ function mostrarHome(){
       <div class="col-sm-4">
         <p class="text-center"><strong>vino 5</strong></p><br>
         <a href="#demo5" data-toggle="collapse">
-          <img src="images/15.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+          <img src="/src/images/19.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
         </a>
         <div id="demo5" class="collapse">
           <p>Drummer</p>
@@ -225,7 +225,7 @@ function mostrarHome(){
       <div class="col-sm-4">
         <p class="text-center"><strong>vino 6</strong></p><br>
         <a href="#demo6" data-toggle="collapse">
-          <img src="images/44.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+          <img src="/src/images/18.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
         </a>
         <div id="demo6" class="collapse">
           <p>Bass player</p>
