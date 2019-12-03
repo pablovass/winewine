800a4e628e6bd447b2303aa259d88beeda7b167b
pablovass <faktory.fm@live.com>
2019-12-03 16:34:19 -0300

    echo los get en productos

--- pages/producto/p.js
+++ pages/producto/p.js
@@ -7,6 +7,3 @@ function hola(){
     console.log("estoy llamando a la funcion ");
 }
 
-//RestApi.get("/api/product")
-    //      .then((msg)=> console.log(msg))
-    //      .catch((err)=>console.error(err))
--- pages/producto/producto.js
+++ pages/producto/producto.js
@@ -1,6 +1,4 @@
-
 class Producto{
-    constructor(){}
     constructor(id,nombre,descripcion,precio,cant_producto,tipo){
         this.id=id;
         this.nombre=nombre;
@@ -8,112 +6,41 @@ class Producto{
         this.precio=precio;
         this.cant_producto=cant_producto;
         this.tipo=tipo;
-        
     }
-    //setter and getter 
-    set id(value){
-        this.id=value;
-    }
-    get id(){
-        return this.id;
-    }
-
-    set nombre(value){
-        this.nombre=value;
-    }
-    get nombre(){
-        return this.nombre;
-    }
-
-    set descripcion(value){
-        this.descripcion=value;
-    }
-    get descripcion(){
-        return this.descripcion;
-    }
-
-    set precio(value){
-        this.precio=value;
-    }
-    get precio(){
-        return this.precio;
-    }
-
-    set cant_producto(value){
-        this.cant_producto=value;
-    }
-    get cant_producto(){
-        return this.cant_producto;
-    }
-    set tipo(value){
-        this.tipo=value;
-    }
-    get tipo(){
-        return this.tipo;
-    }
-
-    //crud
-    //get
-    mostrarDetalleProducto(){
-        fetch(`../api/filter?category=${tipo}&max=${cant_producto}`)
-        .then(data =>data.json())
-        .then(data =>{
-             get=data
-    
-             mostrarDatos(get)
-            })
-        console.log("se muestra la lista de los productos del inventario");
-    
-        console.log("se muestra un detalle del producto seleccionado");
-    }
-    //get
-    mostrarListaProcto(tipo,cant_producto){
-
-        fetch(`../api/filter?category=${tipo}&max=${cant_producto}`)
-        .then(data =>data.json())
-        .then(data =>{
-             get=data
-    
-             mostrarDatos(get)
-            })
-        console.log("se muestra la lista de los productos del inventario");
-    }
-
-    //post
-    cargaDeproducto(){
-        console.log("se suma un nuevo producto al inventario");
-    }
-    //delete
-    descontardelInventario(){
-        console.log("se resta un producto al inventario");
-    }
-    //update
-    ModificarProducto(){
-        console.log("se modifica el producto al inventario");
-    }
-    
-    
 
 }
-    class Vino extends Producto{
-        constructor(){}
-        constructor(id,nombre,descripcion,precio,cant_producto,tipo,tipo_varietal,){
-            super(id,nombre,descripcion,precio,cant_producto,tipo);
-            this.tipo_varietal=tipo_varietal;
-        }
-    }
+/*SETTER AND GETTER */ 
 
-    class Cerveza extends Producto{
-        constructor(){}
-        constructor(id,nombre,descripcion,precio,cant_producto,tipo,tipo_cerveza){
-            super(id,nombre,descripcion,precio,cant_producto,tipo);
-            this.tipo_cerveza=tipo_cerveza;
-        }
-    }
-    class Cerveza extends Producto{
-        constructor(){}
-        constructor(id,nombre,descripcion,precio,cant_producto,tipo,tipo_cerveza){
-            super(id,nombre,descripcion,precio,cant_producto,tipo);
-            this.tipo_cerveza=tipo_cerveza;
-        }
-    }
+
+//post insert
+//setProduct(){
+//    console.log("obtener muchos productos")
+//} 
+//// delete 
+//deleteProduct(){
+//    console.log("obtener muchos productos")
+//}
+//// update 
+//updateproduct(){
+//    console.log("obtener muchos productos")
+//}
+        
+
+//GET
+function getOneProduct(id,tipo){
+    let identificador=id
+    let tipos=tipo 
+    RestApi.get(`/api/filter2?tipo=${tipos}&id=${identificador}`)
+          .then((msg)=> console.log(msg))
+          .catch((err)=>console.error(err))
+}
+
+
+function getManyproduct(tipo,max){
+    let category=tipo
+    let cant=max 
+    
+    RestApi.get(`/api/filter?category=${category}&max=${cant}`)
+          .then((msg)=> console.log(msg))
+          .catch((err)=>console.error(err))
+}
