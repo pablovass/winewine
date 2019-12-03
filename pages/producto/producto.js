6291805b3a19fd419c929876b22d032eb2f26b97
pablovass <faktory.fm@live.com>
2019-12-03 17:20:03 -0300

    echo fetch js para los productos

--- pages/producto/producto.js
+++ pages/producto/producto.js
@@ -40,7 +40,83 @@ function getManyproduct(tipo,max){
     let category=tipo
     let cant=max 
     
-    RestApi.get(`/api/filter?category=${category}&max=${cant}`)
-          .then((msg)=> console.log(msg))
-          .catch((err)=>console.error(err))
+    //RestApi.get(`/api/filter?category=${category}&max=${cant}`)
+          //.then((msg)=> console.log(msg))
+     fetch(`/api/filter?category=${category}&max=${cant}`)   
+          .then((res)=>res.json())
+          .then((data)=>{
+     
+            var losOtros=''
+            //console.log(data) para ver 
+          
+            data.forEach(function(prod) {
+                    losOtros += `
+                                <div class="col-md-4 col-sm-6 col-xs-12">
+                                <div class="blog-post wow fadeInUp" data-wow-delay="0.2s">
+                                <div class="post-img">
+                                <a href="http://localhost/producto_detalle.html?id=${prod.id}"> <img class="img-responsive" src="/images/${prod.id}.jpg" alt="image"></a>
+                                </div>    
+                                <div class="blog-details">
+                                <h4 class="media-heading">${prod.nombre}</h4>
+                                <p>${prod.carasteristica}.</p>
+                                <ul class="list-inline text-capitalize">
+                                <li><b>Oferta $${prod.precio}</b></li>
+                                <li class="pull-right"><span style="text-decoration: line-through;">Antes $${prod.precio}</span></li>
+                            </ul>
+                        </div>
+                    </div>    
+                </div>   
+                `;
+            });
+            document.getElementById('contenido2').innerHTML=losOtros;
+                  
+        })
+        //  .catch((err)=>console.error(err))
 }
+
+/************************************************************************************/
+
+
+//function mostrarDatos(msg){
+//    
+//    msg.forEach(function(prod) {
+//         
+//            product += `
+//                 
+//                 <div class="col-sm-6 col-md-4">
+//                   <div class="thumbnail">
+//                     <img src="/images/${id}.jpg" alt="...">
+//                     <div class="caption">
+//                       <h3>${nombre}</h3>
+//                       <p><b>Oferta $${precio}</b>  <span style="text-decoration: line-through;">Antes $${precio}</span></p>
+//                       <p><a style="margin-left: 67px;" href="producto_detalle.html?id=${id}" class="btn btn-default" role="button">Comprar</a></p>
+//                     </div>
+//                   </div>
+//                 </div>
+//               
+//                 
+//                 `;  
+//        
+//    });
+//    document.getElementById('product').innerHTML=product;
+//    
+//
+//      //  console.log(nombre)
+//        //console.log(carasteristicas)
+//    
+//}
+
+/************************************************************************************/
+
+//botonCerveza=document.getElementById('botonCerveza')
+//botonCerveza.addEventListener('click',function(){
+//    fetch('../api/filter?category=3&max=5')
+//      .then(data =>data.json())
+//      .then(data =>{
+//           get=data
+//  
+//           mostrarDatos(get)
+//          })
+//      })
+//  
+//
