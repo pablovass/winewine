92b09fef4967d8e3b552f352492d8e20719dd05d
pablovass <faktory.fm@live.com>
2019-12-11 17:31:01 -0300

    mi web page

--- inc/footer.js
+++ inc/footer.js
@@ -3,7 +3,7 @@ function getFooter(){
     footer =`<a class="up-arrow" href="#home" data-toggle="tooltip" title="TO TOP">
               <span class="glyphicon glyphicon-chevron-up"></span>
               </a><br><br>
-             <p>desarrollado por Pablo Vallejos <a href="https:pablovallejos.com" data-toggle="tooltip" title="visita a pablo vallejos">pablovallejos.com</a></p>
+             <p>desarrollado por Pablo Vallejos <a href="https:www.pablovallejos.com" data-toggle="tooltip" title="visita a pablo vallejos">pablovallejos.com</a></p>
              <script>
       $(document).ready(function(){
         // Initialize Tooltip
